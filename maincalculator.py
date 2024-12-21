bc834d5137c296e8390e8ade05a5d92e41b7260b
Smikey1 <smikeywork@gmail.com>
2024-12-21 16:17:09 +1100

    :bug: Fixed error

--- maincalculator.py
+++ maincalculator.py
@@ -1,14 +1,1 @@
-import add_work
-import divi_work
-import multiply_work
-import sub_work
-
-a = 100
-b = 50
-
-total_sum = add_work.add(a,b)
-total_sub = sub_work.sub(a,b)
-total_multiply = multiply_work.mul(a,b)
-total_div = divi_work.div(a,b)
-
-print(f"Total sum is {total_sum}, Total Sub is {total_sub}. Total Multiply is {total_multiply}. Total Div is {total_div}")
+#This is by Sandip Dhakal.
